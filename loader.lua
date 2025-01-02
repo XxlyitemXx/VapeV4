0b635fd4eba68b38e95146916b7a641c7b27d4eb
XxlyitemXx <temjaitnt@gmail.com>
2025-01-02 22:12:49 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -10,7 +10,7 @@ end
 local function  downloadFile(path, func)
     if not isfile(path) then
         local suc, res = pcall(function()
-            return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/'..select(1, path:gsub('newvape/', '')), true)
+            return game:HttpGet('https://raw.githubusercontent.com/XxlyitemXx/VapeV4/main'..select(1, path:gsub('newvape/', '')), true)
         end)
         if not suc or res == "404: Not Found" then
             error(res)
